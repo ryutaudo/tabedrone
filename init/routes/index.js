cfdcd8358d34f08020b37a67b842e811cbeb1a1b
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-22 16:31:23 +0900

    add endpoints

--- init/routes/index.js
+++ init/routes/index.js
@@ -2,9 +2,63 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (request, response) => {
-  response.render('index', { title: 'Express' });
+
+/* GET get fridge-content */
+router.get('/fridge-contents/:costumerId', (request, response) => {
+  response.json([
+    {
+      'id': 1, //customer based unique auto-increment. start: 1 
+      'name': 'apple',
+      'amount': 3,
+    },
+    {
+      'id': 2, //customer based unique auto-increment. start: 1 
+      'name': 'orange',
+      'amount': 5,
+    },
+    {
+      'id': 3, //customer based unique auto-increment. start: 1 
+      'name': 'milk',
+      'amount': 2,
+    },
+    {
+      'id': 4, //customer based unique auto-increment. start: 1 
+      'name': 'wine',
+      'amount': 0,
+    },
+  ]);
+});
+
+
+/* GET get order */
+router.post('/order/:costumerId', (request, response) => {
+  /**
+   * is inside the request.
+   * name: cart
+   * value: [{id: 4}, {}]
+   */
+  response.json([
+    {
+      'id': 1, //customer based unique auto-increment. start: 1 
+      'name': 'apple',
+      'amount': 3,
+    },
+    {
+      'id': 2, //customer based unique auto-increment. start: 1 
+      'name': 'orange',
+      'amount': 5,
+    },
+    {
+      'id': 3, //customer based unique auto-increment. start: 1 
+      'name': 'milk',
+      'amount': 2,
+    },
+    {
+      'id': 4, //customer based unique auto-increment. start: 1 
+      'name': 'wine',
+      'amount': 1,
+    },
+  ]);
 });
 
 module.exports = router;
