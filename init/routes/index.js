30282c853599bd5051a2c4504f3b95a8f1c4d166
Ryuta Udo <su101012@gmail.com>
2018-02-22 18:17:06 +0900

    Update index.js

--- init/routes/index.js
+++ init/routes/index.js
@@ -30,35 +30,32 @@ router.get('/fridge-contents/:costumerId', (request, response) => {
 });
 
 
+/* POST post order */
+router.post('/orders', (request, response) => {
+  /**
+   * is inside the request.
+   * customerId: 1
+   * cart: [{name: 'apple', amout: 4}, {...}]
+   */
+  response.json({
+    orderId: 1,
+    customerId: 1,
+    status: 'OPEN',
+  });
+});
+
 /* GET get order */
-router.post('/order/:costumerId', (request, response) => {
+router.get('/orders/:orderId', (request, response) => {
   /**
    * is inside the request.
    * name: cart
-   * value: [{id: 4}, {}]
+   * value: [{name: 'apple', amout: 4}, {...}]
    */
-  response.json([
-    {
-      'id': 1, //customer based unique auto-increment. start: 1 
-      'name': 'apple',
-      'amount': 3,
-    },
-    {
-      'id': 2, //customer based unique auto-increment. start: 1 
-      'name': 'orange',
-      'amount': 5,
-    },
-    {
-      'id': 3, //customer based unique auto-increment. start: 1 
-      'name': 'milk',
-      'amount': 2,
-    },
-    {
-      'id': 4, //customer based unique auto-increment. start: 1 
-      'name': 'wine',
-      'amount': 1,
-    },
-  ]);
+  response.json({
+    orderId: 1,
+    status: 'OPEN', // OPEN / CLOSE
+    cart: [{name: 'apple', amout: 4}],
+  });
 });
 
 module.exports = router;
