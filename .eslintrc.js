41a852c25cc51dfef68ecbc45a9f54d356fd5d2c
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-23 22:37:41 +0900

    updated eslintrc and setup test env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,40 @@
 module.exports = {
-    "extends": "airbnb"
-};
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module",
+    "ecmaVersion": 2017
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 }
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
+    }
+  };
---
+++ src/__tests__/utils/index.js
@@ -0,0 +1,10 @@
+import { getCurrentFridgeContents } from '../../utils/index';
+
+describe('src/utils/index.js', () => {
+  describe('getCurrentFridgeContents', () => {
+    it('should returns contents data when getting contents succeeded.', (done) => {
+      expect(1).toBe(1);
+    });
+  });
+});
+
