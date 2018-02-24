0e67b109e97f449395909fdff1e97ff0df9a4eb8
eliaahadi <elia.ahadi@gmail.com>
2018-02-24 14:55:48 +0900

    1 of 3 endpoint to db, fridge content working

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('../db/index');
 
-
+// tasks to do, Sat Feb 24, 2018
+  // comment out response.json mock data
+  // connect queries to the 3 endpoints below (database to front end)
+  // test if front end gets data using postman on http://localhost:3000/api/...
+ 
 /* GET get fridge-content */
-router.get('/fridge-contents/:costumerId', (request, response) => {
+router.get('/fridge-contents/:costumerId', async (request, response) => {
+  try {
+    const fridgeContents = await db.select('customer_id').table('fridge_inventory');
+    response.send(fridgeContents);
+  }
+  catch(err) {
+    console.error('Error loading locations!', err);
+    response.send(500, 'Internal server error');
+  }
+  /*
   response.json([
     {
       id: 1, // customer based unique auto-increment. start: 1
@@ -27,8 +41,8 @@ router.get('/fridge-contents/:costumerId', (request, response) => {
       amount: 0,
     },
   ]);
+  */
 });
-
 
 /* POST post order */
 router.post('/orders', (request, response) => {
