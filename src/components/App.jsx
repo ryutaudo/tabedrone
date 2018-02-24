c48a7f1cab6a1f5b4bfc19b522b8d02d3d1494dc
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-24 14:20:40 +0900

    implemented utils/getCurrentFridgeContents

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { getCurrentFridgeContents } from '../utils/index';
 
 class App extends Component {
+  async componentDidMount() {
+    const result = await getCurrentFridgeContents();
+  }
+
   render() {
     return (
-      <div className='app'>
-        Hello World!!
+      <div className="app">
+        Hello World!!!!!
       </div>
     );
   }
-};
+}
 
 export default App;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,8 @@
+const getCurrentFridgeContents = async () => {
+  const customerId = 1;
+  const response = await fetch(`api/fridge-contents/${customerId}`);
+  const contents = await response.json();
+  return contents;
+};
 
+export { getCurrentFridgeContents };
