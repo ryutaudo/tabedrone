22a69fff1d725c047994366ac7d05f5ff3415a37
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-24 18:38:25 +0900

    add style to each content

--- src/components/FridgeInventory.jsx
+++ src/components/FridgeInventory.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+  display: 'inline',
+};
+
 class FridgeInventory extends Component {
   componentDidMount() {
     this.props.getFridgeContents();
@@ -11,7 +15,7 @@ class FridgeInventory extends Component {
       return <span> Loading... </span>;
     }
     return this.props.fridgeContents.map(content => (
-      <div className="content" key={content.id}>
+      <div className="content" key={content.id} style={styles}>
         <img
           src={`/img/${content.name}.png`}
           alt={content.name}
