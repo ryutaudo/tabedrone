bca7f579ec782e93f1a04786bd812877894ea992
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-24 15:17:59 +0900

    added case for get fridge contents to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,16 @@
 const defaultState = {
+  fridgeContents: [],
 };
 
-const reducer = (state = defaultState, action) => state;
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GET_FRIDGE_CONTENTS': {
+      return Object.assign({}, state, action.fridgeContents);
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 export default reducer;
