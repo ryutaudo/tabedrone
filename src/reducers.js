52ae5c626df3a691351eceb0251793ccc09e4b97
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-25 18:00:35 +0900

    add reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,7 +3,26 @@ const DefaultState = {
 };
 
 const reducers = (state = DefaultState, action) => {
+  const getCopyFromState = () => JSON.parse(JSON.stringify(state));
   switch (action.type) {
+    case 'ADD_FRIDGE_CONTENT': {
+      const newState = getCopyFromState();
+      if (newState.fridgeContent[action.contentName] === undefined) {
+        newState.fridgeContent[action.contentName] = 0;
+      }
+      newState.fridgeContent[action.contentName] += action.amount;
+      return newState;
+    }
+
+    case 'REMOVE_FRIDGE_CONTENT': {
+      const newState = getCopyFromState();
+      newState.fridgeContent[action.contentName] -= action.amount;
+      if (newState.fridgeContent[action.contentName] < 0) {
+        newState.fridgeContent[action.contentName] = 0;
+      }
+      return newState;
+    }
+
     default:
       return state;
   }
