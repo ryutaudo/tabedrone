1d2b88d594e6292c08dd1828f8de77d3da0f7a36
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-25 18:18:23 +0900

    add + and - icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import Headline from '../container/Headline';
 import OrderButton from '../container/OrderButton';
 import FridgeEntry from '../container/FridgeEntry';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addFridgeEntry = this.addFridgeEntry.bind(this);
+    this.removeFridgeEntry = this.removeFridgeEntry.bind(this);
+  }
 
-const App = ({ fridgeContent }) => (
-  <div>
-    <Headline />
+  addFridgeEntry(event, name = '') {
+    event.preventDefault();
+  }
 
-    <div className="fridge"> 
-    { 
-      Object.keys(fridgeContent).map(name => (<div>
-        name: {name}<br />
-        ammount: {fridgeContent[name]}<br />
-      </div>))
-    }
-    </div>
-    hello world
-    <OrderButton />
-  </div>
-);
+  removeFridgeEntry(event, name = '') {
+    event.preventDefault();
+  }
 
-export default App;
+  render() {
+    return (<div>
+      <Headline />
+
+      <div className="fridge">
+        {
+          Object.keys(this.props.fridgeContent).map((name, idx) => (
+            <div key={idx}>
+              name: {name}<br />
+              ammount: {this.props.fridgeContent[name]}<br />
+              <div id="add-fridge-single-entry" onClick={event => this.addFridgeEntry(event, name)}>
+                 +
+              </div>
+              <div id="remove-fridge-single-entry" onClick={event => this.removeFridgeEntry(event, name)}>
+                 -
+              </div>
+            </div>
+          ))
+        }
+        <div id="add-fridge-entry" onClick={event => this.addFridgeEntry(event)}>
+          +
+        </div>
+      </div>
+      hello world
+      <OrderButton />
+    </div>);
+  }
+}
