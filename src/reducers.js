5869d2efa5b3da9932ade12ce36638613b677bba
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-27 20:32:27 +0900

    added cart state

--- src/reducers.js
+++ src/reducers.js
@@ -4,9 +4,11 @@ const DefaultState = {
     milch: 3,
     hamburger: 1,
   },
+  cart: {},
 };
 
 const reducers = (state = DefaultState, action) => {
+  console.log(state.cart);
   const getCopyFromState = () => JSON.parse(JSON.stringify(state));
   switch (action.type) {
     case 'ADD_FRIDGE_CONTENT': {
@@ -14,15 +16,23 @@ const reducers = (state = DefaultState, action) => {
       if (newState.fridgeContent[action.contentName] === undefined) {
         newState.fridgeContent[action.contentName] = 0;
       }
+      if (newState.cart[action.contentName] === undefined) {
+        newState.cart[action.contentName] = 0;
+      }
       newState.fridgeContent[action.contentName] += action.amount;
+      newState.cart[action.contentName] += 1;
       return newState;
     }
 
     case 'REMOVE_FRIDGE_CONTENT': {
       const newState = getCopyFromState();
       newState.fridgeContent[action.contentName] -= action.amount;
+      newState.cart[action.contentName] -= 1;
       if (newState.fridgeContent[action.contentName] <= 0) {
         delete newState.fridgeContent[action.contentName];
+      }
+      if (newState.cart[action.contentName] <= 0) {
+        delete newState.cart[action.contentName];
       }
       return newState;
     }
