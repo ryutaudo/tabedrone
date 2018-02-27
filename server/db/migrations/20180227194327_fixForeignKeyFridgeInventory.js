fd6491c5554a174a2ff49bbb40f4a0fc3dc5419b
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-27 19:49:24 +0900

    fix migrate file

--- server/db/migrations/20180227194327_fixForeignKeyFridgeInventory.js
+++ server/db/migrations/20180227194327_fixForeignKeyFridgeInventory.js
@@ -1,10 +1,16 @@
 exports.up = (knex) => {
-  knex('fridge_inventory').dropForeign('product_id');
-
+  
+  
   knex.schema.table(
     'fridge_inventory',
-    table => table.foreign('product_id').references('product.id'),
+    (table) => {
+      table.dropForeign('product_id');
+      table.foreign('product_id').references('product.id')
+    },
   );
 };
 
-exports.down = knex => knex('fridge_inventory').dropForeign('product_id');
+exports.down = knex => knex.schema.table(
+  'fridge_inventory',
+  table => table.dropForeign('product_id')
+);
